f479c7bd75f5257f3943e44e442a98699a82c99a
Mirko Lenz <mirko@mirkolenz.com>
2022-07-15 09:43:27 +0200

    Add help to main commands

--- arguebuf_cli/app.py
+++ arguebuf_cli/app.py
@@ -3,8 +3,10 @@ import typer
 from arguebuf_cli import graph, text
 
 cli = typer.Typer()
-cli.add_typer(graph.cli, name="graph")
-cli.add_typer(text.cli, name="text")
+cli.add_typer(
+    graph.cli, name="graph", help="Command group for dealing with argument graphs."
+)
+cli.add_typer(text.cli, name="text", help="Command group for dealing with plain texts.")
 
 if __name__ == "__main__":
     cli()
