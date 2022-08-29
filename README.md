a4719537dab4860f16865e94123808b3bcf47015
Mirko Lenz <mirko@mirkolenz.com>
2022-08-29 15:58:25 +0200

    Update docs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Arguebuf CLI
+
+This project aims at providing some tools to simplify dealing with structured argument graphs.
+Among others, it is possible to convert graphs between different formats, translate them, and render images using graphviz.
+The library can easily be installed using pip:
+
+`pip install arguebuf-cli`
+
+Afterwards, you can execute it by calling `arguebuf`, for example:
+
+`arguebuf --help`
--- arguebuf_cli/app.py
+++ arguebuf_cli/app.py
@@ -4,9 +4,9 @@ from arguebuf_cli import graph, text
 
 cli = typer.Typer()
 cli.add_typer(
-    graph.cli, name="graph", help="Command group for dealing with argument graphs."
+    graph.cli, name="graph", help="Commands for dealing with argument graphs."
 )
-cli.add_typer(text.cli, name="text", help="Command group for dealing with plain texts.")
+cli.add_typer(text.cli, name="text", help="Commands for dealing with plain texts.")
 
 if __name__ == "__main__":
     cli()
